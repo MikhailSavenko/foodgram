665a4b06c2a81982f76c6824aa3adbba8aedd6a6
Михаил Савенко <mi47sav@gmail.com>
2023-10-11 14:47:24 +0300

    Review 4. create_shopping_cart вынесена. Изменена фильтрация

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -1,20 +1,22 @@
 from django_filters import rest_framework as filters
-from recipes.models import Recipe
+from recipes.models import Recipe, Tag
 
 
 class RecipeFilter(filters.FilterSet):
-    tags = filters.CharFilter(
-        field_name='tags__slug', method='filter_tags', required=False
-    )
+    tags = filters.ModelMultipleChoiceFilter(queryset=Tag.objects.all(),
+                                             field_name='tags__slug',
+                                             to_field_name='slug',
+                                             required=False)
     author = filters.NumberFilter(field_name='author__id')
     is_favorited = filters.BooleanFilter(method='filter_is_favorited')
     is_in_shopping_cart = filters.BooleanFilter(
         method='filter_is_in_shopping_cart'
     )
 
-    def filter_tags(self, queryset, name, value):
+    def filter_queryset(self, queryset):
+        queryset_filter = super().filter_queryset(queryset)
         if self.request.user.is_authenticated:
-            return queryset.filter(tags__slug=value)
+            return queryset_filter
         return queryset
 
     def filter_is_favorited(self, queryset, name, value):
